027144f433af6f576fa9d6e57477c1b38832e610
bennybrecher <benbrecher@icloud.com>
2025-01-30 14:11:28 -0500

    nothing fancy yet, just basic gui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+        window.setTitle("Benny's Homework");
+        window.setSize(800,600);
+        window.setLayout(new FlowLayout());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextField textBox1 = new JTextField(20);
+        JTextField textBox2 = new JTextField(20);
+        window.add(textBox1);
+        window.add(textBox2);
+
+        JButton button = new JButton("Add To Label");
+
+        JLabel label = new JLabel("     ");
+        window.add(label);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                label.setText(textBox1.getText()+textBox2.getText());
+            }
+        });
+        window.add(button);
+
+
+        window.setVisible(true);
     }
 }
